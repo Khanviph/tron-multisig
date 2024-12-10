5352ef157a31097b84536cb5df56d4d02d9b74fb
Khanviph <khanviph08@gmail.com>
2024-12-10 18:58:02 +0800

    Initial commit: tron multisig implementation

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+'use client'
+import TronPermissionSetter from '../components/TronPermissionSetter'
+
+export default function Home() {
+  return (
+    <main className="container mx-auto p-4">
+      <TronPermissionSetter />
+    </main>
+  )
+}
